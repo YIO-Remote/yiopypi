8f7d676113b7c7e5db289242605ab13064375d44
Marton <hello@martonborzak.com>
2021-06-23 14:14:10 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
 import os
 from setuptools import setup, find_packages
 
-
 def readme():
     with open("README.md") as f:
         return f.read()
 
-
 PACKAGE_NAME = "yiopypi"
 HERE = os.path.abspath(os.path.dirname(__file__))
-VERSION = "0.0.1"
+VERSION = "0.0.2"
 
 PACKAGES = find_packages(exclude=["tests", "tests.*", "dist", "ccu", "build"])
 
@@ -23,6 +21,8 @@ setup(
     author="Marton Borzak",
     author_email="marton@yio-remote.com",
     description="YIO Remote V2 API python library",
+    long_description=readme(),
+    long_description_content_type='text/markdown',
     packages=PACKAGES,
     include_package_data=True,
     zip_safe=False,
